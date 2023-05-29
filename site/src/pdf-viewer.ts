3f19ec2603c8cc2f6fc6e80bb843ffa0f7569cda
maemon4095 <maemondecocted@gmail.com>
2023-05-29 02:49:57 +0000

    parser

--- site/src/pdf-viewer.ts
+++ site/src/pdf-viewer.ts
@@ -17,6 +17,10 @@ export class PdfViewer extends LitElement {
         const pdfTask = PDF.getDocument(this.src);
 
         const contentsPromise = pdfTask.promise.then(pdf => {
+            Array.from({ length: pdf.numPages }, async (_, i) => {
+                const page = await pdf.getPage(i + 1);
+
+            });
             return html``;
         });
 
@@ -25,3 +29,100 @@ export class PdfViewer extends LitElement {
         return html`<div>${result}</div>`;
     }
 }
+
+// comma separated range patterns.
+// range ::= interger '..' | '..' integer | integer '..' integer | '..' | integer;
+// patterns ::= spaces (range spaces ',' spaces)* range spaces [',' spaces];
+function parseRange(str: string) {
+    const ranges = [];
+    int(str);
+
+    type Parser<T> = (str: string) => [T, string] | null;
+    type AlwaysParser<T> = (str: string) => [T, string];
+    type ResultOf<P extends Parser<any>> = P extends Parser<infer X> ? X : never;
+
+    type ResultsOf<A extends Parser<any>[]> = { [P in keyof A]: ResultOf<A[P]> } & { length: A['length']; };
+
+
+
+    function join<Ps extends Parser<any>[]>(...fs: Ps): Parser<ResultsOf<Ps>> {
+        return (str) => {
+            const results = [];
+
+            for (const f of fs) {
+                const result = f(str);
+                if (result === null) {
+                    return null;
+                }
+
+                results.push(result[0]);
+                str = result[1];
+            }
+
+            return results as ResultsOf<Ps>;
+        };
+    }
+
+    function repeat<T>(f: Parser<T>): AlwaysParser<T[]> {
+        return (str) => {
+            const results: T[] = [];
+
+            while (true) {
+                const result = f(str);
+                if (result == null) {
+                    break;
+                }
+                results.push(result[0]);
+                str = result[1];
+            }
+
+            return [results, str];
+        };
+    }
+
+    function commaToken(str: string): [string, string] | null {
+        const pattern = /^,/;
+        const match = pattern.exec(str);
+        if (match === null) {
+            return null;
+        }
+
+        const token = match[0];
+
+        return [token, str.substring(token.length)];
+    }
+
+    function rangeToken(str: string): [string, string] | null {
+        const pattern = /^\.\./;
+        const match = pattern.exec(str);
+        if (match === null) {
+            return null;
+        }
+        const token = match[0];
+        return [token, str.substring(token.length)];
+    }
+
+    function spaces(str: string): [string, string] {
+        const pattern = /^\s+/;
+        const match = pattern.exec(str);
+        if (match === null) {
+            return ["", str];
+        }
+
+        const blanks = match[0];
+
+        return [blanks, str.substring(blanks.length)];
+    }
+
+    function int(str: string): [number, string] | null {
+        const pattern = /^[0-9]+/;
+
+        const match = pattern.exec(str);
+        if (match === null) {
+            return null;
+        }
+        const intText = match[0];
+        return [parseInt(intText), str.substring(intText.length)];
+    }
+}
+
